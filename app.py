4c4a36181bc7fdcdad791df85341b68e3846bf80
Revanth Reddy Pingala <125516124+Revanth-Reddy-Pingala@users.noreply.github.com>
2024-02-02 13:49:11 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from st_functions import st_button, load_css
-import numpy as np
-from transformers import BertTokenizer, BertForSequenceClassification
-import torch
+
+#Remove these comments if u r running locally in your computer.
+#import numpy as np
+#from transformers import BertTokenizer, BertForSequenceClassification
+#import torch
 
 # Define styles for prediction elements
 prediction_container_style = """
@@ -27,13 +29,19 @@ st.header('Abusive Comment Detector using :blue[_BERT_] :computer:')
 
 st.info('This model is a Finetuned version of BERT on the Abusive Comment Dataset. The dataset contains comments in native Telugu script, Code-Mixed Telugu and Telugu-English Comments. More description can be found in my Github Repo. Enter a comment in the Text Box below and click on Analyze, then the model will give Prediction as Abusive or Not Abusive.')
 
-@st.cache_data
-def get_model():
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model = BertForSequenceClassification.from_pretrained("revanth-reddy-9/Abusive_Comment_Detector_BERT")
-    return tokenizer, model
+st.info(':warning: :red[_Caution_] I am stoping the Deep Learning model to work in this url because it is consuming more RAM which is exceeding Streamlit Community Cloud limits. So I made a video demo of this App Running Locally and uploaded in my Youtube channel.')
 
-tokenizer, model = get_model()
+st_button('youtube', 'https://youtu.be/_oRxPD9kHQg?feature=shared', '   Youtube Video Demo', icon_size)
+
+#Remove these comments if u r running locally in your computer.
+
+#@st.cache_data
+#def get_model():
+#    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+#   model = BertForSequenceClassification.from_pretrained("revanth-reddy-9/Abusive_Comment_Detector_BERT")
+#   return tokenizer, model
+
+#tokenizer, model = get_model()
 
 user_input = st.text_area('Enter Text to Analyze')
 button = st.button("Analyze")
@@ -43,17 +51,19 @@ d = {
     1: 'Not Abusive'
 }
 
-if user_input and button:
-    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512, return_tensors='pt')
-    output = model(**test_sample)
+#Remove these comments if u r running locally in your computer.
+#if user_input and button:
+#   test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512, return_tensors='pt')
+#    output = model(**test_sample)
     #st.write("Logits: ", output.logits)
-    y_pred = np.argmax(output.logits.detach().numpy(), axis=1)
-    prediction_text = f"Prediction: {d[y_pred[0]]}"
-    st.markdown(f"<h2 style='{prediction_text_style}'>{prediction_text}</h2>", unsafe_allow_html=True)
+#   y_pred = np.argmax(output.logits.detach().numpy(), axis=1)
+#   prediction_text = f"Prediction: {d[y_pred[0]]}"
+#   st.markdown(f"<h2 style='{prediction_text_style}'>{prediction_text}</h2>", unsafe_allow_html=True)
 
 icon_size = 24
 
 st_button('linkedin', 'https://www.linkedin.com/in/revanth-reddy-pingala/', '   Connect with me on LinkedIn', icon_size)
 st_button('github', 'https://github.com/Revanth-Reddy-Pingala', '   Check my Github Profile', icon_size)
 st_button('medium', 'https://rrdatadiaries.blogspot.com/', '   Read my Blogs', icon_size)
+st_button('youtube', 'https://www.youtube.com/channel/UCpa8TsplHTVeUbcAv-I4osQ', '   My Youtube Channel', icon_size)
 st_button('instagram', 'https://www.instagram.com/revanth_reddy.1459/', '   Follow me on Instagram', icon_size)
